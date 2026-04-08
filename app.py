6230ef37583fe6dd113078668e9fe4ba52714ab7
Prosperi12 <138193982+Prosperi12@users.noreply.github.com>
2026-04-08 14:49:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+import streamlit as st
+import requests
 
+# Your Supabase info
+SUPABASE_URL = "https://mpgpvcutkibcdhsrbzxh.supabase.co"
+SUPABASE_KEY = "sb_publishable_hJXHy_q1nuntSvyQa8lAiQ_zux_SwWi"
+
+headers = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {sb_publishable_hJXHy_q1nuntSvyQa8lAiQ_zux_SwWi}"
+}
+
+st.title("🇮🇹 Luigi's Italian Bakery")
+
+st.header("Menu")
+
+# READ (GET)
+response = requests.get(
+    f"{SUPABASE_URL}/rest/v1/bakery_items",
+    headers=headers
+)
+
+if response.status_code == 200:
+    items = response.json()
+    for item in items:
+        st.write(f"{item['name']} - ${item['price']} ({item['category']})")
+else:
+    st.write("Error loading items")
